8d6ec1a120f3db478bb375ad1b9ca6bfd1a83bd5
MarleneSalas <201g0614@rcarbonifera.tecnm.mx>
2023-06-11 20:27:58 -0600

    Vistas

---
+++ Views/IndexAdministradorView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WatchMe.Views
+{
+    /// <summary>
+    /// Lógica de interacción para IndexAdministradorView.xaml
+    /// </summary>
+    public partial class IndexAdministradorView : UserControl
+    {
+        public IndexAdministradorView()
+        {
+            InitializeComponent();
+        }
+        private SolidColorBrush selectedButtonBackground = new SolidColorBrush(Color.FromRgb(41, 47, 51));
+        private SolidColorBrush selectedButtonForeground = new SolidColorBrush(Colors.White);
+        private SolidColorBrush noselectedButtonBackground = new SolidColorBrush(Colors.Transparent);
+     
+    }
+}
---
+++ Views/RegistrarLoginView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WatchMe.Views
+{
+    /// <summary>
+    /// Lógica de interacción para RegistrarLoginView.xaml
+    /// </summary>
+    public partial class RegistrarLoginView : UserControl
+    {
+        public RegistrarLoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
