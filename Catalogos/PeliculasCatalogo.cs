597045cfc20c8d2bcd4c377d86f84f3db9859e7c
Rodrigo Elias Castillo <201G0246@rcarbonifera.tecnm.mx>
2023-06-13 00:09:40 -0600

    Adición de catalogos con validaciones

--- Catalogos/PeliculasCatalogo.cs
+++ Catalogos/PeliculasCatalogo.cs
@@ -3,11 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WatchMe.Models;
 
 namespace WatchMe.Catalogos
 {
     public class PeliculasCatalogo
     {
+        WatchMeContext contenedor = new();
+
+        public IEnumerable<Peliculas> GetAllPeliculas()
+        {
+            return contenedor.Peliculas;
+        }
+
+        public Peliculas GetPeliculaXID(Peliculas p)
+        {
+            return contenedor.Peliculas.Find(p.IdPelicula);
+        }
+
+        public void Agregar(Peliculas p)
+        {
+            contenedor.Add(p);
+            contenedor.SaveChanges();
+        }
+
+        public void Eliminar(Peliculas p)
+        {
+            contenedor.Remove(p);
+            contenedor.SaveChanges();
+        }
+
+        public void Editar(Peliculas p)
+        {
+            contenedor.Update(p);
+            contenedor.SaveChanges();
+        }
+
+
+        public bool Validar(Peliculas p, out List<string> Errores)
+        {
+            Errores = new();
+            if (string.IsNullOrWhiteSpace(p.Nombre))
+                Errores.Add("Debe ingresar el titulo de la película.");
+
+            if (p.FechaLanzamiento.Year < 1988)
+                Errores.Add("La fecha de lanzamiento debe ser de una fecha posterior a 1988.");
+
+            if (p.FechaLanzamiento == null)
+                Errores.Add("Debe ingresar la fecha de lanzamiento de la película.");
+
+            if (p.DuracionMinutos < 0)
+                Errores.Add("La duración de la película debe ser mayor a 0.");
+
+            if (string.IsNullOrWhiteSpace(p.Genero))
+                Errores.Add("Debe seleccionar el género de la película.");
+
+            if (string.IsNullOrWhiteSpace(p.Plataformas))
+                Errores.Add("Debe ingresar las plataformas en las que se encuentra disponible dicha película");
+
+            if (string.IsNullOrWhiteSpace(p.Urlposter))
+                Errores.Add("Debe ingresar el póster de la película");
+)
+
+        }
+
+
+
+
 
 
     }
---
+++ Catalogos/ReseñasCatalogo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatchMe.Models;
+
+namespace WatchMe.Catalogos
+{
+    public class ReseñasCatalogo
+    {
+        WatchMeContext contenedor = new();
+
+        public IEnumerable<Reseñas> GetAllReseñas()
+        {
+            return contenedor.Reseñas;
+        }
+
+        public Reseñas GetReseñaXID(Reseñas r)
+        {
+            return contenedor.Reseñas.Find(r.IdProduccion);
+        }
+
+        public void Agregar(Reseñas r)
+        {
+            contenedor.Add(r);
+            contenedor.SaveChanges();
+        }
+
+        public void Eliminar(Reseñas r)
+        {
+            contenedor.Remove(r);
+            contenedor.SaveChanges();
+        }
+        public void Editar(Reseñas r)
+        {
+            contenedor.Update(r);
+            contenedor.SaveChanges();
+        }
+
+        public bool Validar(Reseñas r, out List<string> Errores)
+        {
+            Errores = new();
+            if (string.IsNullOrWhiteSpace(r.Reseña))
+            {
+                Errores.Add("Debe dejar alguna opinión para la película.");
+            }
+            if(r.Valoracion < 0)
+            {
+                Errores.Add("Debe dejar su valoración entre un rango de 0-100");
+            }
+
+            if(r.Reseña != null)
+            {
+                if (r.Reseña.Length > 250)
+                    Errores.Add("La reseña puede contener una longitud máxima de 250 caracteres");
+            }
+
+
+            return Errores.Count == 0;
+        }
+    }
+}
